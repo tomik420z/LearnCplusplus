c1398656fa544dbdcd9c939561cd1d08f4420c89
tomik420z <107411943+tomik420z@users.noreply.github.com>
2022-07-08 00:22:10 +0300

    Add files via upload

---
+++ allocator_construct_traits.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "pool_alloc.h" // my allocator 
+// метафункция, которая проверяет содержит ли аллокатор метод:
+//void allocator<T>::construct(T* ptr, Args&&... args);
+template <typename T, typename AllocatorClass, typename Pointer, typename... Args>
+struct has_method_construct {
+private:
+        static int f(...);
+
+        template<typename U,typename K, typename... Kwargs> 
+        static decltype(std::declval<U>().construct(std::declval<K>() ,std::declval<Kwargs>()...))  
+            f(const U&, const K& , Kwargs&&...);
+public:
+        static constexpr bool value =  std::is_same_v<void, decltype(f(std::declval<AllocatorClass>(), std::declval<Pointer>(), std::declval<Args>()...))> 
+            && std::is_constructible_v<T, Args...>;
+};
+
+template <typename AllocatorClass, typename... Args>
+static const bool has_method_construct_v  = has_method_construct<AllocatorClass, Args...>::value;
+
+//main call!
+// type, container, Args...
+template <typename T, template <typename U> class allocator_container, typename... Args>
+static const bool has_method_construct_alloc_v =  has_method_construct<T, allocator_container<T>, T*, Args...>::value;
+
+int main() {
+    // exapmle
+    std::cout << (has_method_construct_alloc_v<std::string, pool_allocator, std::string>) << std::endl; 
+    return 0;    
+}
